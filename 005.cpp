974cf4a431d1b981a78b52ff38c4427fa419d3a2
Shreya Agarwal <www.shreyaagarwal.com@gmail.com>
2024-01-05 15:09:21 +0530

    005.. Longest Increasing Subs

---
+++ 005.cpp
@@ -0,0 +1,22 @@
+//  Longest Increasing Subsequence
+int lengthOfLIS(vector<int> &nums)
+{
+
+    int n = nums.size();
+    vector<int> temp;
+    temp.push_back(nums[0]);
+
+    for (int i = 1; i < n; i++)
+    {
+        if (nums[i] > temp.back())
+        {
+            temp.push_back(nums[i]);
+        }
+        else
+        {
+            auto ind = lower_bound(temp.begin(), temp.end(), nums[i]);
+            *ind = nums[i];
+        }
+    }
+    return temp.size();
+}

e5ee65e05a25b3af4deff18c47ad6130559f1ed4
Shreya Agarwal <www.shreyaagarwal.com@gmail.com>
2024-01-13 16:05:25 +0530

    013. Minimum Steps Two Strings Anagram

---
+++ 013.cpp
@@ -0,0 +1,15 @@
+// Minimum Number of Steps to Make Two Strings Anagram
+
+int minSteps(string s, string t)
+{
+
+    vector<int> freq(26, 0);
+    for (int i = 0; i < t.size(); i++)
+        freq[t[i] - 'a']++;
+    for (int i = 0; i < s.size(); i++)
+        freq[s[i] - 'a']--;
+    int res = 0;
+    for (int i = 0; i < 26; i++)
+        res += max(0, freq[i]);
+    return res;
+}
